795ce2b89901f372a663e677ad3a52fdcbeb0b58
Aaron Feinberg <aaron.feinberg@wework.com>
2019-01-24 23:48:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -18,5 +18,6 @@ function accessElementInArray(array, index){
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.shift
+  array.shift(element)
+  return array 
 }
